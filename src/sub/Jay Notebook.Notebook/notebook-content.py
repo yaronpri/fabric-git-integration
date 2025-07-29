249379047c3f50e800d374d20cd533a87a96c0a2
Joe Muziki <joemuziki@microsoft.com>
2025-07-29 09:58:19 -0400

    all work items

---
+++ src/sub/Jay Notebook.Notebook/notebook-content.py
@@ -0,0 +1,23 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "environment": {}
+# META   }
+# META }
+
+# CELL ********************
+
+print("Hello Jay in Development Environment")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
