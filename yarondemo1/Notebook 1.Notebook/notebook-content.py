012f447450cb41b6821a8d0f042308df1164dc36
Yaron Pri-Gal <89332819+yaronpri@users.noreply.github.com>
2025-08-11 16:30:07 +0300

    Committing 2 items from workspace 2c23a8af-2b92-4365-972d-068f8cd4d410

--- yarondemo1/Notebook 1.Notebook/notebook-content.py
+++ yarondemo1/Notebook 1.Notebook/notebook-content.py
@@ -6,14 +6,25 @@
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
 # META   },
-# META   "dependencies": {}
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "c0177a0f-ca52-4c1f-99aa-e14b51229f13",
+# META       "default_lakehouse_name": "lake1",
+# META       "default_lakehouse_workspace_id": "2c23a8af-2b92-4365-972d-068f8cd4d410",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "c0177a0f-ca52-4c1f-99aa-e14b51229f13"
+# META         }
+# META       ]
+# META     }
+# META   }
 # META }
 
 # CELL ********************
 
 # Welcome to your new notebook
 # Type here in the cell editor to add code!
-print(1)
+print(22)
 
 # METADATA ********************
 
